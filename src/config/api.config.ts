663e2c11a2b3347087d66bbeb301cf387aef0767
baivab22 <bidaribaivab7@gmail.com>
2025-10-30 15:10:08 +0545

    fix:env fixed

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,12 +1,26 @@
-export const BASE_URL =
-  import.meta.env.MODE === 'development'
-    ? `${import.meta.env.REACT_APP_DEV_URL}`
-    : `${import.meta.env.REACT_APP_PROD_URL}`
+// export const BASE_URL =
+//   import.meta.env.MODE === 'development'
+//     ? `${import.meta.env.REACT_APP_DEV_URL}`
+//     : `${import.meta.env.REACT_APP_PROD_URL}`
 
-export const FILE_URL =
+// export const FILE_URL =
+//   import.meta.env.MODE === 'development'
+//     ? import.meta.env.REACT_APP_DEV_ASSET_URL
+//     : import.meta.env.REACT_APP_DEV_ASSET_URL
+
+// export const TABLE_LIMIT = 10
+// export const MODAL_TABLE_LIMIT = 10
+
+export const BASE_URL =
   import.meta.env.MODE === 'development'
     ? import.meta.env.REACT_APP_DEV_URL
     : import.meta.env.REACT_APP_PROD_URL
 
-export const TABLE_LIMIT = 10
+export const FILE_URL =
+  import.meta.env.MODE === 'development'
+    ? import.meta.env.REACT_APP_DEV_ASSET_URL
+    : import.meta.env.REACT_APP_PROD_ASSET_URL
+
+export const TABLE_LIMIT = import.meta.env.REACT_APP_TABLE_LIMIT || 10
 export const MODAL_TABLE_LIMIT = 10
+
