e39acd67ee72fc87f110a1581b5002767af0ca8b
baivab22 <bidaribaivab7@gmail.com>
2025-10-30 14:42:16 +0545

    fix:env fixed

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,7 +1,7 @@
 export const BASE_URL =
   import.meta.env.MODE === 'development'
-    ? `${import.meta.env.REACT_APP_DEV_URL}/api`
-    : `${import.meta.env.REACT_APP_PROD_URL}/api`
+    ? `${import.meta.env.REACT_APP_DEV_URL}`
+    : `${import.meta.env.REACT_APP_PROD_URL}`
 
 export const FILE_URL =
   import.meta.env.MODE === 'development'
